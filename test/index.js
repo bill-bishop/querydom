be4afeada6f55341953ed841f7751598053a349b
william-bishop <admin@the402.net>
2016-11-21 16:21:05 -0700

    querydom initial commit

---
+++ test/index.js
@@ -0,0 +1,45 @@
+let chai     = require('chai');
+let assert   = require('assert');
+let run      = require("test-cli");
+let stdin    = run.stdin;
+let parseDom = require('../lib/parseDom');
+let cli = require('../lib/cli');
+
+describe('parseDom', function () {
+  it('should parse the dom', function () {
+    let result = parseDom('<div class="lol">Apple</div>', 'div.lol');
+    assert.equal(result.text(), 'Apple');
+  });
+});
+
+describe('CLI', function () {
+  let querydom = run.bind(null, cli);
+
+  it('should read from stdin', function (done) {
+    querydom('span.citrus', stdin('<span class="citrus">Grapefruit</span>'), function (stdout, stderr, code) {
+      assert.equal(stdout, 'Grapefruit\n');
+      done();
+    });
+  });
+
+  it('should read from args only', function (done) {
+    querydom('<div>Orange</div>', 'div', function (stdout, stderr, code) {
+      assert.equal(stdout, 'Orange\n');
+      done();
+    });
+  });
+
+  describe('--attr=*', function () {
+    it('should output the attr value', function (done) {
+      let html = '<div name="billy" class="test">Orange</div>';
+      let selector = 'div.test';
+      let op = '--attr=name';
+      let expectation = 'billy';
+
+      querydom(html, selector, op, function (stdout, stderr, code) {
+        assert.equal(stdout, expectation + '\n');
+        done();
+      });
+    });
+  });
+});
